a469b2d18485e16005956f945b2a91e8f3b69484
chaomingdai <xmcb008@sina.com>
2022-04-25 21:12:35 +0800

    2-2 init

---
+++ src/main/java/com/jiawanew/newwiki/NewwikiApplication.java
@@ -0,0 +1,13 @@
+package com.jiawanew.newwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewwikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewwikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiawanew/newwiki/NewwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiawanew.newwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewwikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
